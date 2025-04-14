8b4b1d948983feb1eb88f729209a321f0e002146
shisonw <122009984+shisonw@users.noreply.github.com>
2025-04-14 12:55:55 +0800

    Create VerifyCard.jsx

---
+++ frontend/src/components/VerifyCard.jsx
@@ -0,0 +1,54 @@
+import React, { useState} from "react";
+import { useNavigate} from 'react-router-dom';
+import '../styles/styles.css';
+import Tooltip from '@mui/material/Tooltip';
+
+
+export const VerifyCard = () => {
+    const [address, setAddress] = useState(" ");
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setAddress(e.target.value);
+    }
+
+    const handleSubmit = () => {
+        console.log('Input value:', address);
+        
+    }
+
+    return (
+        <div >
+            <div className="card-header pb-0 p-3">
+                <div className="row">
+                    <div className="col-6 d-flex align-items-center">
+                        <h4 className="mb-0">Verify Contract</h4>
+                    </div>
+                    <div className="col-6 text-end">
+                        <Tooltip title="Input address before clicking this button!" arrow>
+                            <button onClick={handleSubmit} className="btn bg-gradient-dark mb-0">Verify Now</button>
+                        </Tooltip>
+                    </div>
+
+                </div>
+            </div>
+            <div className=" card-body p-3 ">
+                <div className="row" >
+                    <div className="col-md-12 mb-md-0 mb-4" >
+                        <div>
+                            <Tooltip title="Input address here!" arrow>
+                                <input
+                                    id="input"
+                                    type="text"
+                                    value={address}
+                                    onChange={handleChange}
+                                     />
+                            </Tooltip>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default VerifyCard;
