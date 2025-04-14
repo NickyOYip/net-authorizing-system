780a310fd4d1a6fc69baf4af6ab7cb182d118f3e
shisonw <122009984+shisonw@users.noreply.github.com>
2025-04-14 12:54:18 +0800

    Update SearchCard.jsx

--- frontend/src/components/SearchCard.jsx
+++ frontend/src/components/SearchCard.jsx
@@ -1,66 +1,71 @@
-import React,{useState}from "react";
+import React, { useState } from "react";
+import '../styles/styles.css';
+import Tooltip from '@mui/material/Tooltip';
 
 export const SearchCard = () => {
 
-    const [address,setAddress] = useState(" ");
+  const [address, setAddress] = useState("");
+  const [isOpen, setIsOpen] = useState()
+  const handleChange = (e) => {
+    setAddress(e.target.value);
+  }
 
-    return (
-        <div className="card h-100 mb-4">
-            <div className="card-header pb-0 px-3">
-                <div className="row">
-                    <div className="col-md-4">
-                        <h4 className="mb-0">Find Certificate</h4>
-                    </div>
-                    <div className="col-md-12 d-flex justify-content-start justify-content-md-end align-items-center">
-                        <input
-                            type="text"
-                            placeholder="Input address"
-                            
-                            value={address}
-                            onChange={e => setFormData(prev => ({
-                                ...prev,
-                                certificateName: e.target.value
-                            }))}
-                            style={{borderRadius:"5px",marginRight:"10px",width:"90%",
-                                    height:"100%",background:"whitesmoke",border:"none",
-                                    alignSelf:"left",color:"black"}}
-                            required
-                        />
-                        <button onClick={()=>{
-                            //Search result page(similar to Document management with back button to home page)
-                        }} className="btn bg-gradient-dark mb-0" style={{width:"fit-content"}}>Search</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card-body pt-4 p-3">
-                <ul className="list-group">
-                    {/* ------------SEARCH------------------------------------------------------------------------------------ */}
-                    {/*
-                    {search.map(cert => (
-                      <li className="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                        <div className="d-flex align-items-center">
-                          <div className="d-flex flex-column">
-                            <h6 className="mb-1 text-dark text-sm">{cert.owner}</h6>
-                            <span className="text-xs">{cert.userAddress}</span>
-                          </div>
-                        </div>
-                        <div>
-                          <h6 className="mb-1 text-dark text-sm">{cert.certificateName}</h6>
-                        </div>
-                        <div id="date">
-                          <p>{cert.deployTime}</p>
-                        </div>
-                        <div className="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold">
-                          <p onClick={toView}>Details</p>
-                        </div>
-                      </li>
-                    ))}
-                    */}
-                </ul>
-            </div>
+  const handleSubmit = () => {
+    if (address.length === 0) {
+
+    } else {
+      console.log('Input value:', address);
+    }
+
+  }
+
+  return (
+    <div className="card h-100 mb-4">
+      <div className="card-header pb-0 px-3">
+        <div className="row">
+          <div className="col-md-4">
+            <h4 className="mb-0">Find Contract</h4>
+          </div>
+          <div className="col-md-8 d-flex justify-content-start justify-content-md-end align-items-center">
+            <Tooltip title="Input address before clicking this button!" arrow>
+              <button onClick={handleSubmit}
+                className="btn bg-gradient-dark mb-0" style={{ width: "fit-content" }}>Search</button>
+            </Tooltip>
+          </div>
         </div>
+      </div>
+      <div className=" card-body p-3 ">
+        <div className="row" >
+          <div className="col-md-12 mb-md-0 mb-4" >
+            <div>
+              <Tooltip title="Input address here !" arrow>
+                <input
+                  id="input"
+                  type="text"
+                  value={address}
+                  onChange={handleChange}
+                  required />
+              </Tooltip>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    )
-    
+  )
+};
+
+const styles = {
+  input: {
+    borderRadius: "5px",
+    marginRight: "10px",
+    width: "100%",
+    height: "60px",
+    background: "whitesmoke",
+    border: "none",
+    alignSelf: "left",
+    color: "black",
+    fontSize: "20px"
+  }
 }
 export default SearchCard;
